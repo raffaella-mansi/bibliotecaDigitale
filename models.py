01cbab3599393534d678dce63ea1dc216becce33
raffaella-mansi <raffy949494@gmail.com>
2025-04-12 01:29:04 +0200

    Update models.py

--- models.py
+++ models.py
@@ -10,8 +10,8 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     role = db.Column(db.String(50), nullable=False, default='user')
-    failed_login_count = db.Column(db.Integer, default=0)  # Nuovo campo per tentativi falliti
-    account_locked = db.Column(db.Boolean, default=False)  # Nuovo campo per bloccare l'account
+    failed_login_count = db.Column(db.Integer, default=0)  # campo per tentativi falliti d'accesso
+    account_locked = db.Column(db.Boolean, default=False)  # campo per bloccare l'account
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
